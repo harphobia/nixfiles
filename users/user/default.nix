33ba8424bd3786c22d320505289c846500c7689f
Anhar Bisri <anharbisri@gmail.com>
2025-07-22 11:45:03 +0700

    remove home manager inside user file
    make home manager move to root of flake for easy maintain

--- users/user/default.nix
+++ users/user/default.nix
@@ -1,12 +1,5 @@
 { inputs, lib, config, pkgs, ... }:
-let
-    home-manager = inputs.home-manager;
-in
 {
-    imports = [
-         (import "${home-manager}/nixos")
-    ];
-
     options = {
         users.user.enable =  lib.mkEnableOption "user";
     };
@@ -19,9 +12,5 @@ in
             extraGroups = [ "networkmanager" "wheel" "docker" "gamemode" "adbusers"];
             shell = pkgs.zsh;
         };
-
-        home-manager.useUserPackages = true;
-        home-manager.useGlobalPkgs = true;
-        home-manager.users.user = import ./home.nix;
     };
 }
