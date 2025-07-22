cce7a649b7596ce3b0190ff2881108bcb91bb984
Anhar Bisri <anharbisri@gmail.com>
2025-07-22 11:45:22 +0700

    add stylix to nix flake

--- flake.nix
+++ flake.nix
@@ -1,32 +1,47 @@
 {
-  inputs = {
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
-    };
-  };
+	inputs = {
+		nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+		nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager/release-25.05";
+			inputs.nixpkgs.follows = "nixpkgs-stable";
+		};
+		stylix = {
+			url = "github:nix-community/stylix/release-25.05";
+			inputs.nixpkgs.follows = "nixpkgs-stable";
+		};
+	};
 
-  outputs = { self, ... } @inputs :
-  let
-    system = "x86_64-linux";
-    pkgs = import inputs.nixpkgs-stable {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in
-  {
-    nixosConfigurations.os = inputs.nixpkgs-stable.lib.nixosSystem{
-      inherit system pkgs;
-      specialArgs = { inherit inputs; };
-      modules = [ ./host/os/configuration.nix ];
-    };
+	outputs = { self, ... } @inputs :
+	let
+		system = "x86_64-linux";
+		pkgs = import inputs.nixpkgs-stable {
+			inherit system;
+			config.allowUnfree = true;
+		};
+	in
+	{
+		nixosConfigurations.os = inputs.nixpkgs-stable.lib.nixosSystem{
+			inherit system pkgs;
+			specialArgs = { inherit inputs; };
+			modules = [ 
+				./host/os/configuration.nix
+				inputs.stylix.nixosModules.stylix
+				inputs.home-manager.nixosModules.home-manager {
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.user = import ./users/user/home.nix;
+				}
+			];
+		};
 
-    homeConfigurations.user = inputs.home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = { inherit inputs; };
-      modules = [ ./users/user/home.nix ];
-    };
-  };
+		homeConfigurations.user = inputs.home-manager.lib.homeManagerConfiguration {
+			inherit pkgs;
+			extraSpecialArgs = { inherit inputs; };
+			modules = [
+				inputs.stylix.nixosModules.stylix 
+				./users/user/home.nix
+			];
+		};
+	};
 }
