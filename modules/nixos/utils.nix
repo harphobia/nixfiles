7d89f4e176c09d81fc81edc25808303a077147a9
harphobia <35424466+harphobia@users.noreply.github.com>
2025-09-09 09:33:04 +0700

    switch to chromium

--- modules/nixos/utils.nix
+++ modules/nixos/utils.nix
@@ -1,7 +1,7 @@
 { config, pkgs, inputs, ... }:
 {
     environment.systemPackages = with pkgs; [
-        	unrar
+        unrar
 		unzip
 		p7zip
 		jq
@@ -14,11 +14,25 @@
 		ffmpeg
 		arrpc
 		home-manager
-    		git
+    	git
     ];
     
 	programs.adb.enable = true;
 	
 	programs.obs-studio.enableVirtualCamera = true;
 	boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback.out ];
+
+	programs.chromium = {
+		enable = true;
+		extensions = [
+			"cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+			"ghmbeldphafepmbegfdlkpapadhbakde" # proton pass
+			"jinjaccalgkegednnccohejagnlnfdag" # violentmonkey
+		];
+		extraOpts = [
+			"BrowserSignin" = 0;
+  			"SyncDisabled" = true;
+			"PasswordManagerEnabled" = false;
+		];
+	};
 }
