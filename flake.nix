cfebb3e3f8bbcb8826a3231d56df17efc61c52e5
Anhar Bisri <anharbisri@gmail.com>
2026-09-06 19:40:38 +0700

    fix syntax

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     home-manager.url = "github:nix-community/home-manager/release-26.05";
+
     niri-flake.url = "github:sodiboo/niri-flake";
     noctalia.url = "github:noctalia-dev/noctalia";
     helium.url = "github:schembriaiden/helium-browser-nix-flake";
